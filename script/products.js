7fbb1bb3215462c098b74b8da2bb73baf6c2b51d
Natalia Niedziela <n.niedziela201@gmail.com>
2022-12-29 17:40:48 +0100

    fix stars

--- script/products.js
+++ script/products.js
@@ -119,7 +119,7 @@ function getProdOpinion(prodId){
             });
                 const rating = document.querySelectorAll("#opinionRating");
                for(let z = 0; z <= opinions.length; z++){
-                     for(let i = 0; i <= opinions[z].rating; i++){
+                     for(let i = 0; i < opinions[z].rating; i++){
                          rating[z].innerHTML += '<svg version="1.1" class="opinionsSection__opinions__article__svg" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"viewBox="0 0 280.124 280.124" style="enable-background:new 0 0 280.124 280.124;" xml:space="preserve"><g><path style="fill:#FFE880;" d="M280.124,106.914l-92.059-6.598L140.057,4.441l-48.55,95.874L0,106.914l61.282,74.015l-17.519,94.754l96.294-43.614l96.294,43.606l-17.799-94.754C218.553,180.919,280.124,106.914,280.124,106.914z"/><polygon style="fill:#FFE880;" points="236.352,275.683 218.553,180.92 280.071,106.975 280.071,106.905 188.065,100.315 140.057,4.441 140.057,232.068 	"/></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg>';
                      }
                      
